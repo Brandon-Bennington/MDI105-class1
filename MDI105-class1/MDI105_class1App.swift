261bf045f24e1f069251ace4def97c5724da75d2
Brandon <brandonwbennington@gmail.com>
2025-08-06 22:00:49 -0300

    Initial Commit

---
+++ MDI105-class1/MDI105_class1App.swift
@@ -0,0 +1,17 @@
+//
+//  MDI105_class1App.swift
+//  MDI105-class1
+//
+//  Created by Brandon Bennington on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MDI105_class1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
